f2285e8fa6a32bc2ea7ed6c37afd2169ccac82bc
Mimma Akter Omi <mimma.omi@gmail.com>
2024-07-09 23:26:51 +0600

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -56,16 +56,7 @@ const Header = () => {
                     </Link>
                     <button className="flex justify-end md:hidden ring-1 ring-black rounded" onClick={showNav}>
                       <i className="fas fa-bars text-white w-9 h-9 flex justify-center items-center hover:text-black"></i>
-                    </button>
-                    <div className={`${toggle ? " flex" : " hidden"}flex items-center lg:order-2`}>
-
-                        <Link
-                            to="#"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Get started
-                        </Link>
-                    </div>
+                    </button>                   
                     <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                         <ul className={`${toggle ? " flex" : " hidden"} flex-col justify-center items-center w-full first:mt-2 md:flex-row md:w-auto md:space-x-10 md:flex`}>
                                 { //dynamic menu
@@ -85,6 +76,9 @@ const Header = () => {
 
                         </ul>
                     </div>
+                    <button className={`${toggle ? " flex" : " hidden"} text-indigo-800 hover:bg-gray-300 mx-auto md:mx-0 md:flex md:mt-0 items-center justify-center font-medium bg-gray-100 px-1 p-2 rounded-lg mt-4 w-24`}>
+                       Get Started 
+                    </button>
                 </div>
             </nav>
         </header>
