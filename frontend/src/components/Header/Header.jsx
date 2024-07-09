11052b8edd8923181b2d35bf209a930e0e7b2c45
Mimma Akter Omi <37964763+MimmaAkter@users.noreply.github.com>
2024-07-09 23:03:21 +0600

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,6 +6,11 @@ import {useSelector} from 'react-redux'
 const Header = () => {
     const authStatus = useSelector((state) => state.auth.status)
     const navigate = useNavigate()
+
+    const [toggle, setToggle] = useState(false);
+    const showNav = () => {
+        setToggle(!toggle);
+      };
   
     const navItems = [
       {
@@ -49,6 +54,9 @@ const Header = () => {
                             alt="Logo"
                         />
                     </Link>
+                    <button className="flex justify-end md:hidden ring-1 ring-black rounded" onClick={showNav}>
+                      <i className="fas fa-bars text-white w-9 h-9 flex justify-center items-center hover:text-black"></i>
+                    </button>
                     <div className="flex items-center lg:order-2">
 
                         <Link
@@ -59,7 +67,7 @@ const Header = () => {
                         </Link>
                     </div>
                     <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                        <ul className={`${toggle ? " flex" : " hidden"} flex-col justify-center items-center w-full first:mt-2 md:flex-row md:w-auto md:space-x-10 md:flex`}>
                                 { //dynamic menu
                                     navItems.map((item) => 
                                         item.active ? (
