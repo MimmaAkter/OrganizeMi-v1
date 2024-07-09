d948fc3770db75df04adadaa72a13f39d227700b
Mimma Akter Omi <37964763+MimmaAkter@users.noreply.github.com>
2024-07-09 21:27:44 +0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      //'/api':'http://localhost:5050'
       '/api':{
-        //target: 'https://organizemi-v1-server.onrender.com',
-        target: 'http://localhost:5050',
-        changeOrigin: true
-      }
+        target:'https://organizemi-v1-server.onrender.com',
+        changeOrigin:true
+      } 
     },
   },
   plugins: [react()],
