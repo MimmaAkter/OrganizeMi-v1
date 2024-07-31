02424b1365cb46427c549e36a1a786b9d5c90c20
Mimma Akter Omi <37964763+MimmaAkter@users.noreply.github.com>
2024-07-31 16:00:42 +0600

    Update profile.service.js

--- frontend/src/services/profile.service.js
+++ frontend/src/services/profile.service.js
@@ -4,7 +4,7 @@ const Read = async () => {
     const URL = "/api/v1/profile/Read"
     const result = await axios.get(URL);
     console.log(result)
-    return result.data.reverse();
+    return result.data.data.reverse();
   };
 
 const ReadById = async (id) => {
